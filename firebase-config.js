c2c6eb2a98b71e929a8244579282d3f557c340d5
adynu182 <adynu182@gmail.com>
2026-09-05 21:37:58 +0700

    update page

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+// PERINGATAN: file ini JANGAN di-commit ke Git / repo publik (sudah masuk .gitignore).
+// Taruh file ini satu folder dengan dashboard-obat.html.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA5VeYSTXmBpceDwbqIddooSdaIB9maDg8",
+  authDomain: "iuran-db.firebaseapp.com",
+  projectId: "iuran-db",
+  storageBucket: "iuran-db.firebasestorage.app",
+  messagingSenderId: "380835597026",
+  appId: "1:380835597026:web:402b8d837243c3e2dbecbb"
+};
